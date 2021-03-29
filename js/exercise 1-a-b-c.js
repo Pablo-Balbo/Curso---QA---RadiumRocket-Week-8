e44a00716de579b753223e58f7fc52841dd075d2
Pablo Balbo <pabloabalbo@hotmail.com>
2021-03-29 18:06:55 -0300

    Those are the answers to the exercises

---
+++ js/exercise 1-a-b-c.js
@@ -0,0 +1,40 @@
+// EJERCICIO 1-A
+
+// Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+
+// var num1 = 2;
+// var num2 = 4;
+// var sum = num1 + num2;
+
+// console.log('2 + 4 =', sum);
+
+// ---------------------------------------------------------------------------------------------------------------------------------
+
+// EJERCICIO 1-B
+
+// Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+
+// var string1 = 'Oid mortales,';
+// var string2 = 'el himno sagrado.';
+// var himno = string1, string2;
+
+// console.log(string1, string2);
+
+// ----------------------------------------------------------------------------------------------------------------------------------
+
+// EJERCICIO 1-C
+
+// Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) 
+// guardando el resultado la suma una 3er variable (utilizar length).
+
+var string1 = 'Pablo';
+var string2 = 'Balbo';
+
+var result1 = string1.length;
+var result2 = string2.length;
+
+var final_result = result1 + result2;
+
+console.log(final_result);
+
+// -----------------------------------------------------------------------------------------------------------------------------------
