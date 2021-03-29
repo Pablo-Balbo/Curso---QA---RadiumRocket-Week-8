77f2e328501e61085b04d626ffb4a2a5108f974f
Pablo Balbo <pabloabalbo@hotmail.com>
2021-03-29 18:08:27 -0300

    Those are the answers to exercise three

---
+++ js/exercise 3-a-b-c-d-e-f-g.js
@@ -0,0 +1,87 @@
+// EXERCISE 3-A
+
+// Dado el siguiente array: 
+// ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+// mostrar por consola los meses 5 y 11 (utilizar console.log)
+
+// array = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+// console.log(array [4], array [10]);
+
+// --------------------------------------------------------------------------------------------------------------------------------
+
+// EXERCISE 3-B
+
+// Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).
+
+// array = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+// array = array.sort();
+
+// console.log(array);
+
+// --------------------------------------------------------------------------------------------------------------------------------
+
+// EXERCISE 3-C
+
+// Agregar un elemento al principio y al final del array (utilizar unshift y push).
+
+// array = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+// first_change = array.unshift('Pablo');
+// second_change = array.push('Balbo');
+
+// console.log(array);
+
+// --------------------------------------------------------------------------------------------------------------------------------
+
+// EXERCISE 3-D
+
+// Quitar un elemento del principio y del final del array (utilizar shift y pop).
+
+// array = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+// delete_first_element = array.shift();
+// delete_ultimate_element = array.pop();
+
+// console.log(array);
+
+// --------------------------------------------------------------------------------------------------------------------------------
+
+// EXERCISE 3-E
+
+// Invertir el orden del array (utilizar reverse)
+
+// array = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+// reverse = array.reverse();
+
+// console.log(array);
+
+// --------------------------------------------------------------------------------------------------------------------------------
+
+// EXERCISE 3-F
+
+// Unir todos los elementos del array en un único string donde cada mes este separado por un guión - (utilizar join).
+
+// array = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+// console.log(
+//     array.join('-')
+// );
+
+// --------------------------------------------------------------------------------------------------------------------------------
+
+// EXERCISE 3-F
+
+// Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar slice).
+
+array = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+copy = array.slice(4, 11);
+
+console.log(
+    copy = array.slice(4, 11)
+);
+
+// --------------------------------------------------------------------------------------------------------------------------------
